0ea25dbbd6e985965ab3ccb11d212c1821803c7a
CrazyVito11 <31567466+CrazyVito11@users.noreply.github.com>
2021-03-02 16:54:39 +0100

    Begin of new homepage added

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <header class="flex ">
+            <div class="w-screen">
+                <div class="relative shadow-2xl-thick sm:overflow-hidden rounded-md">
+                    <div class="absolute inset-0">
+                        <img class="h-full w-full object-cover" src="https://i.pinimg.com/originals/45/cf/a8/45cfa8e8fef45c356253cf9938a4cc9c.jpg" alt="">
+                        <div class="absolute inset-0 bg-indigo-100" style="mix-blend-mode: multiply;"></div>
+                    </div>
+                    <div class="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
+                        <h1 class="text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
+                            The world wide web.
+                        </h1>
+                        <p class="mt-6 max-w-lg text-xl sm:max-w-3xl">
+                            Websites en webapplicaties zijn belangrijke dingen voor moderne bedrijven.<br/>
+                            Een goede website kan je bedrijf beter zichtbaar maken
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </header>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    //import HelloWorld from './components/HelloWorld.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            //HelloWorld
+        }
+    };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './assets/tailwind.scss'
+import './assets/app.scss'
 
 Vue.config.productionTip = false;
 
